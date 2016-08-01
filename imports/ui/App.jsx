dbc3f4b0cda680aae678fab53b789b41ffb344bc
Fran Dios <Fran Dios>
2016-08-01 19:02:38 +0900

    Basic ToDo app with Meteor.

---
+++ imports/ui/App.jsx
@@ -0,0 +1,93 @@
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.jsx';
+
+// App component - represents the whole app
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text via the React ref
+    const text = this.refs.textInput.value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    this.refs.textInput.value = '';
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
+  renderTasks() {
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+
+    return filteredTasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
+  };
+}, App);
---
+++ server/main.js
@@ -0,0 +1,9 @@
+
+import { Meteor } from 'meteor/meteor';
+
+import '../imports/api/tasks.js';
+
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
